2e67d0df3b4364bc5b4a76ad6e50c476970f9a7c
NeutrinoSae <kneotrino@yahoo.com>
2019-06-07 20:24:17 +0800

    jadi

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
       <p>Jalan Jenderal Sudirman No.37B-D, Kuanino, Kec. Kota Raja, Kota Kupang, Nusa Tenggara Tim. 85119</p>
     </div>
     <div>
-      <h3><span>Connect</span> with us</h3>
+      <h3>Connect with us</h3>
       <a href="https://www.facebook.com/HapPupKaraoke/" id="facebook">Facebook</a> 
       <a href="https://twitter.com/official_happup" id="twitter">Twitter</a> </div>
     <p>Copyright &copy; 2020</p>
